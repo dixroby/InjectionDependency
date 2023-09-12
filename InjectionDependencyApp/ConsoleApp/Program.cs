a77f8ea7c554bf7b3e46a4d520e86b4304941b95
dixroby <dixroby.arone@touchconsulting.pe>
2023-09-11 21:52:00 -0500

    Refactor: Implementacion con Host.

--- InjectionDependencyApp/ConsoleApp/Program.cs
+++ InjectionDependencyApp/ConsoleApp/Program.cs
@@ -2,22 +2,42 @@
 using ConsoleApp;
 using MemoryDataSource;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Repositories;
 using UseCases;
 
+
+/// con servicios
+/// ==================================================================
+
 // service collection es donde se agrega los servicios
-IServiceCollection serviceCollection = new ServiceCollection();
+//IServiceCollection serviceCollection = new ServiceCollection();
 
-// Registrar repository
-serviceCollection.AddSingleton<ProductRepository>();
-serviceCollection.AddSingleton<ProductsDataSource>();
-serviceCollection.AddSingleton<GetProductInteractor>();
-serviceCollection.AddSingleton<GetProductController>();
+//// Registrar repository
+//serviceCollection.AddSingleton<ProductRepository>();
+//serviceCollection.AddSingleton<ProductsDataSource>();
+//serviceCollection.AddSingleton<GetProductInteractor>();
+//serviceCollection.AddSingleton<GetProductController>();
 
-// DependencyInjection, construye el service provider
-IServiceProvider serviceProvider =  serviceCollection.BuildServiceProvider();
+//// DependencyInjection, construye el service provider
+//IServiceProvider serviceProvider =  serviceCollection.BuildServiceProvider();
 
-// Pedir el servicio que deseas
-GetProductController controller = serviceProvider.GetService<GetProductController>();
+//// Pedir el servicio que deseas
+//GetProductController controller = serviceProvider.GetService<GetProductController>();
+
+//controller.GetProduct();
+
+/// con HOST
+/// ==================================================================
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddSingleton<ProductRepository>();
+builder.Services.AddSingleton<ProductsDataSource>();
+builder.Services.AddSingleton<GetProductInteractor>();
+builder.Services.AddSingleton<GetProductController>();
+
+using IHost AppHost = builder.Build();
+
+var controller = AppHost.Services.GetRequiredService<GetProductController>();
 
 controller.GetProduct();
